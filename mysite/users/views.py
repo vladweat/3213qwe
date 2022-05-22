f97400b616ef5f5b089f9d7876a56b02c6eeebcb
VladWeat <kononov.vw@gmail.com>
2022-05-22 17:08:30 +0300

    web3 setup +

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,4 @@
+from distutils.command.config import config
 from django.shortcuts import redirect, render
 from django.http import (
     HttpResponse,
@@ -15,9 +16,13 @@ from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, LoginForm
 
 from web3 import Web3, HTTPProvider
+from dotenv import dotenv_values
 
-bank_address = "0xB5dea2661dfa4b8f0aaE8Bc1F583369D927c3b06"
-bank_private_key = "0xcad32d1bbf83420e30b80cbe1f944dfe510db0af34ab42f0341b67f5f7da530a"
+config = dotenv_values(".env")
+
+# bank address
+bank_address = config.get("BANK_ADDRESS")
+bank_private_key = config.get("BANK_PRIVATE_KEY")
 
 # Create your views here.
 def index(request):
@@ -27,6 +32,7 @@ def index(request):
         return render(request, "index.html")
 
 
+# login function
 def login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
@@ -61,30 +67,34 @@ def check_web3(request):
         return HttpResponseRedirect("creating_proposal")
 
 
+# logout func that redirect on /login
 @login_required
 def logout(request):
     django_logout(request)
     return HttpResponseRedirect("/login")
 
+
+# function that send 0.005 eth from bank_account to account
 def send_from_bank_to_account(user):
     web3 = Web3(
         HTTPProvider("https://rinkeby.infura.io/v3/32a867e993e44d8bbd973382f147e060")
     )
     nonce = web3.eth.getTransactionCount(bank_address)
-    
+
     tx = {
         "nonce": nonce,
         "to": user.eth_address,
         "value": web3.toWei(0.005, "ether"),
         "gas": 2000000,
-        "gasPrice": web3.toWei('50', 'gwei')
+        "gasPrice": web3.toWei("50", "gwei"),
     }
-    
+
     signed_tx = web3.eth.account.sign_transaction(tx, bank_private_key)
     tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
     print(web3.toHex(tx_hash))
 
 
+# main register function
 def registr(request):
     if request.method == "POST":
         user_form = CustomUserCreationForm(request.POST)
@@ -97,12 +107,12 @@ def registr(request):
 
             new_user.private_key = private_key
             new_user.eth_address = account.address
-
+            # saving new_user
             new_user.set_password(user_form.cleaned_data["password1"])
             new_user.save(update_fields=["private_key", "eth_address"])
-            
+            # adding some eth on register
             send_from_bank_to_account(new_user)
-            
+
             return HttpResponsePermanentRedirect("/login")
             # return render(request, "users/login_page.html", {"new_user": new_user})
     else:
